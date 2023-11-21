aeaf4028f8be9004acd17ade3e7ed38e60f76451
Andrew Haarsager <athaarsager@gmail.com>
2023-11-21 15:48:27 -0600

    Create tentative operate function

--- javascript.js
+++ javascript.js
@@ -17,3 +17,9 @@ function multiplyNumbers(num1, num2) {
 function divideNumbers(num1, num2) {
     return num1 / num2;
 }
+
+function operate(operator, firstNumber, secondNumber) {
+    //change value of operator based on clicked button
+    //run the selected function
+    //firstNumber, secondNumber, and operator as inputs to this function?
+}
